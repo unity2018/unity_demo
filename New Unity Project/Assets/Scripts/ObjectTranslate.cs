527223bd6fd89b6cdcc77869c99d4ebfd546ebc1
unity2018 <adesh.tiwari@girnarsoft.com>
2018-07-23 12:08:51 +0530

    log msg updated...........

--- New Unity Project/Assets/Scripts/ObjectTranslate.cs
+++ New Unity Project/Assets/Scripts/ObjectTranslate.cs
@@ -6,7 +6,7 @@ public class ObjectTranslate : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        print("Object Translater Initialised!");
 	}
 	
 	// Update is called once per frame
